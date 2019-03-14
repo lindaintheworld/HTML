bdad42621a42d386ada5fed275e80eb59c8a164b
lindaintheworld <iaminkyung2@gmail.com>
2019-03-14 16:06:52 +0900

    03/14 정리

---
+++ jspProject_추가/web/resources/js/main.js
@@ -0,0 +1,13 @@
+/** main.js 
+ * 2019-02-12 작성
+ * 자바스크립트 테스트용(자바 스크립트 소스 파일 : 하나의 스크립트 태그)
+ */
+
+function testShow(){
+	window.alert("main.js 안에 있는 testShow() 실행됨");
+}
+
+function changeImage(){
+	document.getElementById("image1").src = "/testj/resources/images/Android.gif"
+}
+
